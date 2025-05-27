cab98d8aac6320952f4e4eae21a392a46fa2eef9
Cristobal Espinoza P <crispinochet@outlook.cl>
2025-05-26 22:51:36 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+let randWho = who[Math.floor(Math.random()* who.length)]
+let randAction = action [Math.floor(Math.random()* action.length)]
+let randWhat = what[Math.floor(Math.random()* what.length)]
+let randWhen = when[Math.floor(Math.random()* when.length)]
+
+let excuse = randWho + " " + randAction + " " + randWhat + " " + randWhen
+let p = document.querySelector("#excuse")
+p.innerHTML = excuse
