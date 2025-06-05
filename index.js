2cbaee7f1541c4d7ba05cecf3bb5ef8adc96a205
Cristobal Espinoza P <crispinochet@outlook.cl>
2025-06-04 21:27:28 -0400

    refactorizacion de codigo y buenas practicas

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-let randWho = who[Math.floor(Math.random()* who.length)]
-let randAction = action [Math.floor(Math.random()* action.length)]
-let randWhat = what[Math.floor(Math.random()* what.length)]
-let randWhen = when[Math.floor(Math.random()* when.length)]
+let randomWho = who[Math.floor(Math.random()* who.length)]
+let randomAction = action [Math.floor(Math.random()* action.length)]
+let randomWhat = what[Math.floor(Math.random()* what.length)]
+let randomWhen = when[Math.floor(Math.random()* when.length)]
 
-let excuse = randWho + " " + randAction + " " + randWhat + " " + randWhen
+let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`
 let p = document.querySelector("#excuse")
 p.innerHTML = excuse
